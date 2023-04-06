b12e5937db8c826a93da9c25375c7a0a753f00e8
DawsonFuhr <95586105+DawsonFuhr@users.noreply.github.com>
2023-04-05 20:18:49 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
-function preload()
-{
-
-}
-
 function setup()
 {
-    canvas = createCanvas(640, 480);
+    canvas = createCanvas(480, 480);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
 
-    tint_color="";
+    
     
 }
 
-function draw() {
-    image(video, 0, 0, 640, 480);
-    tint(tint_color);
-}
+
 
 function take_snapshot() {
     save('student_name.png');
 }
 
-function filter_tint()
+function draw_shape()
 {
-    tint_color = document.getElementById("color_name").value;
+    
+  
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+      
+    
+
+    var rand = getRandomInt(3);
+
+
+    image(video, 0, 0, 640, 480);
+
+    if (rand == 0) {
+        circle(30, 30, 20);
+        describe('white circle with black outline in mid of gray canvas');
+    }
+   
+    if (rand == 1) {
+        ellipse(56, 46, 55, 55);
+        describe('white ellipse with black outline in middle of a gray canvas');
+    }
+
+if (rand == 2) {
+    rect(30, 20, 55, 55);
+    describe('white rect with black outline in mid-right of canvas');
+}
+
+
+
+
 }
